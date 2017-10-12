550b37ab3b10e58058349438a1364c3d35acfb03
LOGOMERKEZ\Deniz.Atak <deniz.atak@logo.com.tr>
2017-10-12 17:11:21 +0300

    modified pom

--- src/main/java/com/lbs/data/demo/courses/CourseRepository.java
+++ src/main/java/com/lbs/data/demo/courses/CourseRepository.java
@@ -3,11 +3,13 @@ package com.lbs.data.demo.courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
 
 import java.util.List;
 //deneme
 //public interface CourseRepository extends JpaRepository<Course, String>, JpaSpecificationExecutor<Course> {//interface olması ne kadar ilginç
+   @Repository
     public interface CourseRepository extends JpaRepository<Course, String>, JpaSpecificationExecutorWithProjection<Course> {//interface olması ne kadar ilginç
 
     public List<CourseWithoutParent> findByTopicIsNull();
--- src/test/java/com/lbs/data/demo/DemoApplicationTests.java
+++ src/test/java/com/lbs/data/demo/DemoApplicationTests.java
@@ -2,7 +2,6 @@ package com.lbs.data.demo;
 
 import com.lbs.data.demo.courses.Course;
 import com.lbs.data.demo.courses.CourseRepository;
-import com.lbs.data.demo.courses.CourseRepository2;
 import com.lbs.data.demo.courses.CourseSpec;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -10,12 +9,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest (classes = DemoApplication.class)
@@ -23,10 +19,6 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private CourseRepository courseRepository;
-
-	@Autowired
-	private CourseRepository2 courseRepository2;
-
 
 	private CourseSpec courseSpec;
 
@@ -36,7 +28,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void specificationWithProjection() {
-		Specification<Course> where= Specifications.where(CourseSpec.idEq("java"));
+		Specification<Course> where= Specifications.where(CourseSpec.idEq("Spring Kursu"));
 		Page<CourseRepository.CourseWithoutParent> all = courseRepository.findAll(where,CourseRepository.CourseWithoutParent.class, null);
 		Assertions.assertThat(all).isNotEmpty();
 		System.out.println(all.getContent());
@@ -49,3 +41,5 @@ public class DemoApplicationTests {
 //
 //	}
 }
+
+
